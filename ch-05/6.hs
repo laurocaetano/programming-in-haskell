efa3a2aaa81618969b1785a8f374a78e927dda0f
Lauro Caetano <laurocaetano1@gmail.com>
2014-07-17 20:29:26 -0300

    No need for list comprehension here.

--- ch-05/6.hs
+++ ch-05/6.hs
@@ -1,7 +1,7 @@
 -- Redefine the function positions using the function find.
 
 positions      :: Eq a => a -> [a] -> [Int]
-positions x xs = [i | i <- find x (zip xs [0..n])]
+positions x xs = find x (zip xs [0..n])
                  where n = length xs - 1
 
 find     :: Eq a => a -> [(a, b)] -> [b]
